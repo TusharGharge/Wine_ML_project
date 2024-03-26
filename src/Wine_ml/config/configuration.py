6fea1d683773c94726f940584fcf8108d2c32648
TusharGharge <tushargharge1111@gmail.com>
2024-03-26 13:59:16 +0530

    model evalution and ui

--- src/Wine_ml/config/configuration.py
+++ src/Wine_ml/config/configuration.py
@@ -1,6 +1,6 @@
 from src.Wine_ml.constants import *
 from src.Wine_ml.utils.common import read_yaml,create_directories
-from src.Wine_ml.entity.config_entity import (DataIngestionConfig,ModelTrainerConfig,DataTransformationConfig,DataValidationConfig)
+from src.Wine_ml.entity.config_entity import (DataIngestionConfig,ModelTrainerConfig,DataTransformationConfig,DataValidationConfig,ModelEvaluationConfig)
 class ConfigurationManager:
     def __init__(
             self,
@@ -77,3 +77,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/TusharGharge/Wine_ML_project.mlflow",
+           
+        )
+
+        return model_evaluation_config
